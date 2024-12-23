3d6cde102f63d6b16e5506537bfac70824a509fe
Gleb <kovgleb04@gmail.com>
2024-12-24 00:32:23 +0300

    fixed cat

--- ConcerteApp/Scene/TextedCat/TextedCatViewController.swift
+++ ConcerteApp/Scene/TextedCat/TextedCatViewController.swift
@@ -9,10 +9,9 @@ import UIKit
 
 class TextedCatViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
     
     @IBOutlet weak var imageView: UIImageView!
-    
-    @IBOutlet weak var catText: UILabel!
     
     @IBOutlet weak var descriptionLabel: UILabel!
     
@@ -26,27 +25,89 @@ class TextedCatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        catText.text = ""
         descriptionLabel.text = "Generate your cat"
         activityIndicator.layer.opacity = 0
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapView))
+        view.addGestureRecognizer(gestureRecognizer)
+                
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification , object:nil)
+            
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification , object:nil)
+    }
+    
+    @objc private func didTapView() {
+        view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        let keyboardHeight = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.height
+        
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
     
     private func downloadCat() {
+        guard let text = textField.text, !text.isEmpty else {
+            return
+        }
+            
+        let encodedText = text.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
+            
+        guard let url = URL(string: "https://cataas.com/cat/says/\(encodedText)") else {
+            return
+        }
+            
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let data = data, error == nil else {
+                DispatchQueue.main.async { [weak self] in
+                    self?.descriptionLabel.text = "Text cat failed downloading"
+                    self?.activityIndicator.layer.opacity = 0
+                    self?.button.isEnabled.toggle()
+                    self?.textField.text = ""
+                    self?.textField.isEnabled.toggle()
+                }
+                return
+            }
+                
+            DispatchQueue.main.async { [weak self] in
+                self?.imageView.image = UIImage(data: data)
+                self?.descriptionLabel.text = "Text cat was downloaded"
+                self?.activityIndicator.layer.opacity = 0
+                self?.button.isEnabled.toggle()
+                self?.textField.text = ""
+                self?.textField.isEnabled.toggle()
+            }
+        }
+            
+        task.resume()
+    }
+    
+    private func downloadRandomCat() {
         guard let url = URL(string: "https://cataas.com/cat") else {
             return
         }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data else {
+            
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let data = data, error == nil else {
+                DispatchQueue.main.async { [weak self] in
+                    self?.descriptionLabel.text = "Random cat failed downloading"
+                    self?.activityIndicator.layer.opacity = 0
+                    self?.button.isEnabled.toggle()
+                    self?.textField.text = ""
+                    self?.textField.isEnabled.toggle()
+                }
                 return
             }
             
             DispatchQueue.main.async { [weak self] in
                 self?.imageView.image = UIImage(data: data)
-                self?.descriptionLabel.text = "Cat was downloaded"
+                self?.descriptionLabel.text = "Random cat was downloaded"
                 self?.activityIndicator.layer.opacity = 0
                 self?.button.isEnabled.toggle()
-                self?.catText.text = self?.textField.text ?? ""
                 self?.textField.text = ""
                 self?.textField.isEnabled.toggle()
             }
@@ -57,13 +118,16 @@ class TextedCatViewController: UIViewController {
     
     
     @IBAction func didTapButton(_ sender: Any) {
-        catText.text = " "
         descriptionLabel.text = " "
         activityIndicator.layer.opacity = 1
         button.isEnabled.toggle()
         textField.isEnabled.toggle()
         descriptionLabel.text = "Start downloading cat"
-        downloadCat()
+        if (textField.text == "") {
+            downloadRandomCat()
+        } else {
+            downloadCat()
+        }
     }
 }
 
